9524e9ae8d82d5bdd7148fa934b2602cd5acaaf4
Fabio <fabioborgesouzajunior@gmail.com>
2024-04-02 20:58:40 -0300

    :lipstick: style: Adds style to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,22 @@
-import Logo from "assets/logoFabio.svg"
+import Logo from "assets/logoNicolas.svg"
 import TreeDots from "assets/treeDots.png"
 import ThemeToggle from "assets/themeToggle.png"
 
 export function Header(){
     return(
-        <div>
-            <img src={Logo} alt="logo"></img>
-            <h2>Services</h2>
-            <h2>Portfolios</h2>
-            <h2>Experience</h2>
-            <h2>Blog</h2>
-            <img src={TreeDots} alt="Options" />
-            <img src={ThemeToggle} alt="Theme Toggle" />
+        <div className="flex flex-wrap justify-around w-full mt-5">
+            <div className="flex items-center gap-2">
+                <img className="m-2" src={Logo} alt="logo"></img>
+                <h1>Nicolas</h1>
+            </div>
+            <div className="flex flex-wrap items-center gap-16">
+                <h2>Services</h2>
+                <h2>Portfolios</h2>
+                <h2>Experience</h2>
+                <h2>Blog</h2>
+                <img src={TreeDots} alt="Options" />
+            </div>
+            <img className="mx-8" src={ThemeToggle} alt="Theme Toggle" />
         </div>
     )
 }
