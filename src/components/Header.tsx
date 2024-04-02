ac483592f295019cd9b96d9faf54d98ebefab812
Fabio <fabioborgesouzajunior@gmail.com>
2024-04-02 20:02:28 -0300

    :sparkles: feat: Adds html elements to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
+import Logo from "assets/logoFabio.svg"
+import TreeDots from "assets/treeDots.png"
+import ThemeToggle from "assets/themeToggle.png"
+
 export function Header(){
     return(
         <div>
-            <image href="/logo192.png"></image>
+            <img src={Logo} alt="logo"></img>
+            <h2>Services</h2>
+            <h2>Portfolios</h2>
+            <h2>Experience</h2>
+            <h2>Blog</h2>
+            <img src={TreeDots} alt="Options" />
+            <img src={ThemeToggle} alt="Theme Toggle" />
         </div>
     )
 }
