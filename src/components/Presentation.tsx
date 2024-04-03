c588d260efb411aae24354d115a13632f9a6e7be
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-04-02 21:31:17 -0300

    feat: icons

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,19 +1,47 @@
 import Github from "../assets/Github.svg"
+import Cellphone from "../assets/Telephone.svg"
+import Email from "../assets/mail.svg"
+import Location from "../assets/location-outline.svg"
+import LinkedIn from "../assets/Linkedin.svg"
 
 function Presentation() {
     return (
-        <div className="container mx-auto flex flex-row flex-wrap justify-between border-2">
+        <div className="container mx-auto flex flex-row justify-between border-2">
             <div className="w-2/5">
                 <p className="font-serif text-lg text-gray-400">MY NAME IS</p><br />
-                <p className="font-bold text-3xl">Nicolas Montoro Rodrigues</p><br />
-                <p className="text-lg">Creative front-end developer with more than +5 years of experience in enterprise companies and startups. Proficient in JavaScript, Angular, and React. Passionate about UI/UX</p><br />
-                <img src={Github}></img>
+                <p className="font-bold text-3xl">Nicolas Montoro Rodrigues.</p><br />
+                <p className="text-lg">Creative front-end developer with more than +5 years of experience in enterprise companies and startups. Proficient in JavaScript, Angular, and React. Passionate about UI/UX.</p><br />
+                
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                                <img src={Github} alt="Github"></img>
+                            </th>
+                            <th>
+                                <img src={LinkedIn} alt="LinkedIn"></img>
+                            </th>                            
+                        </tr>
+                    </thead>
+                    <tbody>                                                
+                        <img 
+                            src={Cellphone} 
+                            alt="Cellphone">                        
+                        </img>
+                        <img 
+                            src={Email} 
+                            alt="Email">                        
+                        </img>
+                        <img 
+                            src={Location} 
+                            alt="Location">                        
+                        </img>
+                    </tbody>                    
+                </table>
+                
             </div>
             <div className="w-2/5">
-                <img
-                    src="https://engepesca.com.br/uploads/imagens/800x600_tilapia-pesquisadores-descobrem-na-composicao-creatina-taurina-e-glutamato-249-7424.jpg"
-                    alt="logo">
-                </img>
+                <img src="https://engepesca.com.br/uploads/imagens/800x600_tilapia-pesquisadores-descobrem-na-composicao-creatina-taurina-e-glutamato-249-7424.jpg" alt="logo"></img>
             </div>
         </div>
     )
