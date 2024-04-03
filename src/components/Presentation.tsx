a81349197a629b1088c06de4dba5c67e41387a9a
Nicolas Montoro Rodrigues <nicolasmrod93@gmail.com>
2024-04-03 13:53:18 -0300

    feat: adjusting table height

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,52 +1,52 @@
 import Github from "../assets/Github.svg"
-import Cellphone from "../assets/Telephone.svg"
+import Cellphone from "../assets/telephone.svg"
 import Email from "../assets/mail.svg"
 import Location from "../assets/location-outline.svg"
 import LinkedIn from "../assets/Linkedin.svg"
 
 export default function Presentation() {
     return (
-        <div className="container mx-auto flex flex-row justify-between border-2">
+        <div className="container mx-auto flex justify-between">
             <div className="w-2/5">
                 <p className="font-serif text-lg text-gray-400">MY NAME IS</p><br />
                 <p className="font-bold text-3xl">Nicolas Montoro Rodrigues.</p><br />
                 <p className="text-lg">Creative front-end developer with more than +5 years of experience in enterprise companies and startups. Proficient in JavaScript, Angular, and React. Passionate about UI/UX.</p><br />
                 
-                <table>
-                    <thead>
-                        <tr>
-                            <th className="pr-8">
-                                <img src={Github} alt="Github"></img>
+                <table className="w-full border-4">
+                    <thead className="border-4">
+                        <tr className="border-4">
+                            <th>
+                                <img src={Github}   alt="Github"></img>
                             </th>
-                            <th className="pr-8">
+                            <th>
                                 <img src={LinkedIn} alt="LinkedIn"></img>
                             </th>                            
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <td className="pt-4">
+                    <tbody className="border-4">
+                        <tr className="border-4">
+                            <td>
                                 <img src={Cellphone} alt="Cellphone"></img>
                             </td>
-                            <td className="pt-4">(XX) XXXXX-XXXX</td>                          
+                            <td>(XX) XXXXX-XXXX</td>                          
                         </tr>
-                        <tr>
-                            <td className="pt-4">
+                        <tr className="border-4">
+                            <td>
                                 <img src={Email} alt="Email"></img>
                             </td>
-                            <td className="pt-3">tilapia@gmail.com</td>                          
+                            <td>tilapia@gmail.com</td>                          
                         </tr>
-                        <tr>
-                            <td className="pt-4">
+                        <tr className="border-4">
+                            <td>
                                 <img src={Location} alt="Location"></img>
                             </td>
-                            <td className="pt-3">Location</td>                          
+                            <td>Location</td>                          
                         </tr>
                     </tbody>                    
                 </table>
                                 
             </div>
-            <div className="w-2/5">
+            <div className="text-center w-2/5">
                 <img src="https://engepesca.com.br/uploads/imagens/800x600_tilapia-pesquisadores-descobrem-na-composicao-creatina-taurina-e-glutamato-249-7424.jpg" alt="logo"></img>
             </div>
         </div>
