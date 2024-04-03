56f0e68f87e057bc61c1f8066126e2c2340dc6ae
Fabio <fabioborgesouzajunior@gmail.com>
2024-04-03 19:08:32 -0300

    :sparkles: feat: Gets routes to elements with <a> element. Adds button to theme toggle.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,13 +10,15 @@ export function Header(){
                 <h1>Nicolas</h1>
             </div>
             <div className="flex flex-wrap items-center gap-16">
-                <h2>Services</h2>
-                <h2>Portfolios</h2>
-                <h2>Experience</h2>
-                <h2>Blog</h2>
+                <a href="/services">Services</a>
+                <a href="/portfolio">Portfolios</a>
+                <a href="/experience">Experience</a>
                 <img src={TreeDots} alt="Options" />
             </div>
-            <img className="mx-8" src={ThemeToggle} alt="Theme Toggle" />
+            <button>
+                <img className="mx-8" src={ThemeToggle} alt="Theme Toggle" />
+            </button>
+            
         </div>
     )
 }
