823cd2db4dfc1fc412564ce3a5b082f8c40c1ead
Fabio <fabioborgesouzajunior@gmail.com>
2024-04-03 19:42:15 -0300

    :broom: cleanup: Gets off solids border

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,17 +4,17 @@ import ThemeToggle from "assets/themeToggle.png"
 
 export function Header(){
     return(
-        <div className="flex flex-wrap justify-around w-full mt-5 border-2 border-red-600">
-            <div className="flex items-center gap-2 border-2 border-green-600">
+        <div className="flex flex-wrap justify-around w-full mt-5">
+            <div className="flex items-center gap-2">
                 <img className="m-2" src={Logo} alt="logo"></img>
                 <h1>Nicolas</h1>
             </div>
-            <div className="flex flex-wrap items-center gap-6 sm:gap-16 border-2 border-yellow-600 w-full sm:w-auto justify-center">
+            <div className="flex flex-wrap items-center gap-8 sm:gap-16 w-full sm:w-auto justify-center">
                 <a href="/services">Services</a>
                 <a href="/portfolio">Portfolios</a>
                 <a href="/experience">Experience</a>
                 <img src={TreeDots} alt="Options" />
-                <button className="border-2 border-black">
+                <button>
                     <img src={ThemeToggle} alt="Theme Toggle" />
                 </button>            
             </div>
