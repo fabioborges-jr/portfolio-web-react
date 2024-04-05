8c6910ed4e03775fdd1e4d6b9500b339dd2b7467
Fabio <fabioborgesouzajunior@gmail.com>
2024-04-04 22:36:29 -0300

    :lipstick: style: Adds background color in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "background-1":"#FFFFFF",
+        "background-2":"#FBFBFB"
+      }
+    },
   },
   plugins: [],
 }
