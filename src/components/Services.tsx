b2587d84b631615c152809d6c2e0304ce03381cc
Fabio <fabioborgesouzajunior@gmail.com>
2024-04-04 22:23:27 -0300

    :sparkles: feat: Creates HTML elements

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,5 +1,29 @@
+import { FaBeer } from 'react-icons/fa';
+
 export default function Services(){
     return(
-        <h1>Hello World</h1>
+        <div>
+            <div>
+                <p>Services</p>
+                <h1>Specialized in</h1>
+            </div>
+            <div>
+                <div>
+                    <FaBeer/>
+                    <h2>Lorem ipsum</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur totam reprehenderit a in inventore ex itaque. In cumque</p>
+                </div>
+                <div>
+                    <FaBeer/>
+                    <h2>Lorem ipsum</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur totam reprehenderit a in inventore ex itaque. In cumque</p>
+                </div>
+                <div>
+                    <FaBeer/>
+                    <h2>Lorem ipsum</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur totam reprehenderit a in inventore ex itaque. In cumque</p>
+                </div>
+            </div>
+        </div>
     )
 }
