4c578ace833a092ec6b7f3918dcf996278c437d5
Fabio <fabioborgesouzajunior@gmail.com>
2024-04-06 00:27:10 -0300

    :iphone: style: Responsives the component

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,29 +1,38 @@
-import { FaBeer } from 'react-icons/fa';
+import { FaBeer } from 'react-icons/fa'
+import { IconContext } from "react-icons"
 
 export default function Services(){
     return(
-        <div>
-            <div>
-                <p>Services</p>
-                <h1>Specialized in</h1>
+        <IconContext.Provider value={{size:"1.5em", color:"#7E74F1"}}>
+            <div className='flex flex-col items-center text-center py-16 bg-background-2'>
+                <div className='text-left'>
+                    <p>Services</p>
+                    <h1>Specialized in</h1>
+                </div>
+                <div className='flex flex-col items-center sm:flex-row'>
+                    <div className='w-5/6 mt-4 p-12 bg-background-1 rounded-md mb-2'>
+                        <div className='w-fit mx-auto p-6 bg-service-bg rounded-2xl'>
+                            <FaBeer/>
+                        </div>
+                        <h2 className='my-3'>Lorem ipsum</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur totam reprehenderit a in inventore ex itaque. In cumque</p>
+                    </div>
+                    <div className='w-5/6 mt-4 p-12 bg-background-1 rounded-md mb-2'>
+                        <div className='w-fit mx-auto p-6 bg-service-bg rounded-2xl'>
+                        <FaBeer/>
+                        </div>
+                        <h2 className='my-3'>Lorem ipsum</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur totam reprehenderit a in inventore ex itaque. In cumque</p>
+                    </div>
+                    <div className='w-5/6 mt-4 p-12 bg-background-1 rounded-md'>
+                        <div className='w-fit mx-auto p-6 bg-service-bg rounded-2xl'>
+                            <FaBeer/>
+                        </div>
+                        <h2 className='my-3'>Lorem ipsum</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur totam reprehenderit a in inventore ex itaque. In cumque</p>
+                    </div>
+                </div>
             </div>
-            <div>
-                <div>
-                    <FaBeer/>
-                    <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur totam reprehenderit a in inventore ex itaque. In cumque</p>
-                </div>
-                <div>
-                    <FaBeer/>
-                    <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur totam reprehenderit a in inventore ex itaque. In cumque</p>
-                </div>
-                <div>
-                    <FaBeer/>
-                    <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur totam reprehenderit a in inventore ex itaque. In cumque</p>
-                </div>
-            </div>
-        </div>
+        </IconContext.Provider>
     )
 }
